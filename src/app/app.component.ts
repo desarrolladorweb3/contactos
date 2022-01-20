fb8b27dc0535673925869d3928a2d82184eba311
Julian Rojas <julianrjs15@gmail.com>
2022-01-20 08:52:19 -0500

    Maqueta inicial del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'contacts';
+  constructor() { }
+  dtOptions: any = {};
+  is_lucot_first_register = true;
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      language: {
+        url: '//cdn.datatables.net/plug-ins/1.10.11/i18n/Spanish.json'
+      },
+    };
+  }
 }
